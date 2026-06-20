9cd5a48f309a7861dc5ecb41e040c52ef2396c94
PICHISTORM <davidalejandrot522@gmail.com>
2026-06-20 16:41:40 -0600

    se agregaron cambios en los estilos generales ,y se cumplieron los requisitos de home,perfil y usuarios

--- src/app/pages/perfil/perfil.ts
+++ src/app/pages/perfil/perfil.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsuariosService, Usuario } from '../../core/services/usuarios.service';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './perfil.html',
   styleUrl: './perfil.scss',
 })
 export class Perfil {
 
+  private UsuariosService = inject(UsuariosService);
+  usuarios: Usuario[] = [];
+   cargando = true;
+
+
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.UsuariosService.obtenerUsuarios().subscribe(data => {
+        this.usuarios = data;
+        this.cargando = false;
+      });
+    }, 5000);
+  }
+
 }
+
+
+
