ac4e2454b0bc2e531b532ee4eacf6e8a1652a7b3
EmmaRichi <maguelmimonmario@gmail.com>
2026-06-20 22:44:19 -0600

    se agrego los estilos a las pantallas que faltaban

--- src/app/pages/documentacion/documentacion.ts
+++ src/app/pages/documentacion/documentacion.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { PagesRoutingModule } from "../pages-routing-module";
 
 @Component({
   selector: 'app-documentacion',
-  imports: [],
+  imports: [PagesRoutingModule],
   templateUrl: './documentacion.html',
   styleUrl: './documentacion.scss',
 })
+
 export class Documentacion {
+
+  programas = [
+
+    {
+      id:1,
+      nombre:"NodeJS",
+      imagen:"https://mindsparktechnologies.com/wp-content/uploads/2025/05/nodejs.png",
+      link:"https://nodejs.org/docs/latest/api/"
+    },
+
+
+    {
+      id:2,
+      nombre:"TypeScript",
+      imagen:"https://raw.githubusercontent.com/remojansen/logo.ts/master/ts.png",
+      link:"https://www.typescriptlang.org/docs/"
+    },
+
+
+    {
+      id:3,
+      nombre:"Angular",
+      imagen:"https://angular.io/assets/images/logos/angular/angular.svg",
+      link:"https://angular.dev/"
+    },
+
+
+    {
+      id:4,
+      nombre:"Ionic",
+      imagen:"https://trbl-services.eu/wp-content/uploads/2018/06/ionic-1.png",
+      link:"https://ionicframework.com/docs"
+    },
+
+
+    {
+      id:5,
+      nombre:"npm",
+      imagen:"https://blog.berkankutuk.dk/tech/npm.png",
+      link:"https://docs.npmjs.com/"
+    }
+
+
+  ];
 
 }
--- src/app/pages/estructuras/estructuras.ts
+++ src/app/pages/estructuras/estructuras.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+interface Directiva {
+  nombreA: string;
+  nombreB: string;
+  descripcion: string;
+  imagen: string;
+  alte: string;
+}
+
 
 @Component({
   selector: 'app-estructuras',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './estructuras.html',
   styleUrl: './estructuras.scss',
 })
+
+
 export class Estructuras {
+
+
+directivas: Directiva[] = [
+
+  {
+    nombreA: '*ngIf',
+    nombreB: '@if',
+    descripcion:
+    'Permite mostrar o esconder elementos del HTML dependiendo de una condición lógica.',
+    imagen: 'https://pbs.twimg.com/media/GG2XBymWcAA34aJ.jpg',
+    alte: 'ngIf'
+  },
+
+
+  {
+    nombreA: '*ngFor',
+    nombreB: '@for',
+    descripcion:
+    'Permite recorrer listas y generar elementos repetidos en HTML.',
+    imagen: 'https://media.licdn.com/dms/image/v2/D5622AQHQ7z9ux7ExNQ/feedshare-shrink_480/B56Zfg5jshG0AY-/0/1751824874782?e=2147483647&v=beta&t=XzPJBjXG9w156jEUwn7cL-DpQ-OOIVBIBwhcE3gideA',
+    alte: 'ngFor'
+  },
+
+
+  {
+    nombreA: '*ngSwitch',
+    nombreB: '@switch',
+    descripcion:
+    'Permite mostrar contenido dependiendo del valor de una variable.',
+    imagen: 'https://www.angulartraining.com/daily-newsletter/wp-content/uploads/2023/08/image-14.png',
+    alte: 'ngSwitch'
+  }
+
+];
+
 
 }
